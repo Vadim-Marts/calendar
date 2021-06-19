d91c3dc5d40e814ca05c6372560195646a41f473
Vadym Martseniuk <martsenykvadim@gmail.com>
2021-06-19 11:39:04 +0300

    change html srtucture main calendar

--- app/js/main.js
+++ app/js/main.js
@@ -8,24 +8,21 @@ const   togleSidebarButton = document.querySelector('.top-nav__main-menu-btn'),
 togleSidebarButton.addEventListener('click', togleSidebar);
 
 createSmallCalendar(currentDate);
-
-
+createMainCalendar(currentDate);
 
 arrowButtons.addEventListener('click', (event) => {
     const button = event.target.offsetParent.classList.value;
 
     switch (button) {
         case 'prev-btn' : 
-            setPrevMonth(currentDate);
+            changeMonth();
             break;
         case 'next-btn' : 
-            setNextMonth(currentDate);
+            changeMonth(true);
             break;
     }       
     
 });
-
-
 
 
 
@@ -35,53 +32,57 @@ function togleSidebar () {
     sidebar.classList.toggle('sidebar--hide');
 }
 
+{
+    let i = 1;
 
-function getMonth(argDate) { 
-    let month;
-    
-    month = argDate.getMonth();
-    return month;
+    function changeMonth (isNext) {
+        let year = currentDate.getFullYear();
+        let month = currentDate.getMonth();
+        let newDate = new Date(year, month);
+
+        isNext ? i++ : i--;
+
+        newDate.setMonth(newDate.getMonth() + i - 1);
+
+        createSmallCalendar(newDate);
+        createMainCalendar(newDate);
+    }
+
 }
-function getYear(argDate) { argDate
-    let year;
-    
-    year = argDate.getFullYear();
-    return year;
-}
+
 
 function createSmallCalendar (argDate) {
-    const monthName = document.querySelector('.current__month'),
-            yearName = document.querySelector('.current__year'),
+    const monthName = document.querySelectorAll('.current__month'),
+            yearName = document.querySelectorAll('.current__year'),
             smallCalendarDays = document.querySelector('.sm-calendar__days'),
-            smallCalendarElemDayClass = 'sm-calendar__day',
-            dayNotThisMonthClass = 'dayNotThisMonth',
-            currentDayClass = 'current_day';
+            cellClasses = {
+                default : 'sm-calendar__day',
+                inactive : 'dayNotThisMonth',
+                current : 'current_day'
+            };
 
-    let year = getYear(argDate);
-    let month = getMonth(argDate);
+    let year = argDate.getFullYear();
+    let month = argDate.getMonth();
     let date = new Date(year, month);
     let content = '';
-    let arr = [];
 
-    for (let i = 0; i < date.getDay(); i++) {
-        let date = new Date(year, month, 1);
-        date.setDate(date.getDate() - i -1);
-        
-        content = `<div class="${smallCalendarElemDayClass} ${dayNotThisMonthClass}">${date.getDate()}</div>`;
-
-        arr.push(content);
+    for (let i = date.getDay(); i > 0; i--) {
+        let date = new Date(year, month, 0)
+               
+        content += `<div class="${cellClasses.default} ${cellClasses.inactive}">${ date.getDate() - i + 1}</div>`;
     }
-    content = arr.reverse().join('');
-
 
     while (date.getMonth() === month) {
-        const dateString = date.toLocaleDateString().split(",")[0];
-        const currentDateString = currentDate.toLocaleDateString().split(",")[0];
+        let arr1 = [];
+        const dateString = arr1.join(arr1.push(date.getFullYear() + '' + date.getMonth() + date.getDate()));
+        arr1 = [];
+        const currentDateString = arr1.join(arr1.push(currentDate.getFullYear() + '' + currentDate.getMonth() + currentDate.getDate()));
+
         if (dateString === currentDateString) {
-            content += `<div class="${smallCalendarElemDayClass} ${currentDayClass}"> ${date.getDate()}</div>`;
+            content += `<div class="${cellClasses.default} ${cellClasses.current}"> ${date.getDate()}</div>`;
             date.setDate(date.getDate() + 1);
         }
-        content += `<div class="${smallCalendarElemDayClass}"> ${date.getDate()}</div>`;
+        content += `<div class="${cellClasses.default}"> ${date.getDate()}</div>`;
         date.setDate(date.getDate() + 1);
     }
 
@@ -91,44 +92,54 @@ function createSmallCalendar (argDate) {
             let dates = new Date(year, month + 1, date.getDate());
             date.setDate(date.getDate() + j);
             
-            content += `<div class="${smallCalendarElemDayClass} ${dayNotThisMonthClass}">${dates.getDate()}</div>`;
+            content += `<div class="${cellClasses.default} ${cellClasses.inactive}">${dates.getDate()}</div>`;
             j++;
         }
     }
 
     smallCalendarDays.innerHTML = content;
-    monthName.innerText = monthsArr[month]; 
-    yearName.innerText = year;
+
+    for (let i = 0; i < 2; i++) {
+        monthName[i].innerText = monthsArr[month]; 
+        yearName[i].innerText = year;
+    }
 
 } 
 
+function createMainCalendar (argDate) {
+    const calendarCells = document.querySelectorAll('.bg-calendar__day');
 
+    let year = argDate.getFullYear();
+    let month = argDate.getMonth();
+    let date = new Date(year, month);
+    let daysArr = [];
 
-{
-    let i = 1;
-
-    function setNextMonth (argDate) {
-        let year = getYear(argDate);
-        let month = getMonth(argDate);
-        let newDate = new Date(year, month);
-        
-        newDate.setMonth(newDate.getMonth() + i);
-        i++;
-
-        createSmallCalendar(newDate)
+    for (let i = date.getDay(); i > 0; i--) {
+        let date = new Date(year, month, 0)
+        daysArr.push(date.getDate() - i + 1);
     }
 
-    function setPrevMonth (argDate) {
-        let year = getYear(argDate);
-        let month = getMonth(argDate);
-        let newDate = new Date(year, month);
-        
-        --i;
-        newDate.setMonth(newDate.getMonth() + i - 1);
+    while (date.getMonth() === month) {
 
-        createSmallCalendar(newDate);
+        daysArr.push(date.getDate());
+        date.setDate(date.getDate() + 1);
     }
+
+    if (date.getDate() !== 0) {
+        for (let i = calendarCells.length - daysArr.length; i > 0; i--) {
+            let j = 1;
+            daysArr.push(date.getDate());
+            date.setDate(date.getDate() + j);
+            j++;
+        }
+    }
+
+    for (let i = 0; i < daysArr.length; i++){
+        calendarCells[i].innerText = daysArr[i];
+    }
+ 
 }
+
 
 
 
